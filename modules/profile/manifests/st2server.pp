5fe34ae9610a66d0b765ece229ec0dd049d06cce
James Fryman <james@fryman.io>
2015-01-27 10:08:19 -0600

    moving to upstream managed profile

--- modules/profile/manifests/st2server.pp
+++ modules/profile/manifests/st2server.pp
@@ -1,15 +1,3 @@
 class profile::st2server {
-  class { '::st2':
-    version  => hiera('st2::version', '0.6.0'),
-  }
-  include '::st2::stanley'
-
-  class { '::st2::profile::mongodb': }
-  -> class { '::st2::profile::rabbitmq': }
-  -> class { '::st2::profile::nodejs': }
-  -> class { '::st2::role::client': }
-  -> class { '::st2::role::mistral':
-    manage_mysql => true,
-  }
-  -> class { '::st2::role::server': }
+  include ::st2::profile::fullinstall
 }
