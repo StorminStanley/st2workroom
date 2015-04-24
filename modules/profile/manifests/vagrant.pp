f0169a7e022f92281db41a3f336f09e4e702003d
James Fryman <james@fryman.io>
2015-04-25 00:04:21 +0200

    Miscellaneous RHEL Fixes
    While doing some development on the RHEL platform, a few changes
    needed to be made in order to load up the environment properly.
    
    * Upgrades Puppetfile to include latest st2 module (0.2.5)
    * Adds RHEL specific packages for Vagrant Datacenter Profile
    * Disables Hubot for RHEL 6 machines

--- modules/profile/manifests/vagrant.pp
+++ modules/profile/manifests/vagrant.pp
@@ -1,6 +1,17 @@
 class profile::vagrant {
-  $packages = ['avahi-daemon', 'ruby-dev']
-  package { $packages:
+  $_packages = $::osfamily ? {
+    'Debian' => ['avahi-daemon', 'ruby-dev'],
+    'RedHat' => ['avahi', 'ruby-devel'],
+  }
+
+  package { $_packages:
     ensure => present,
+  }
+
+  service { 'avahi-daemon':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
   }
 }
--- modules/role/manifests/st2express.pp
+++ modules/role/manifests/st2express.pp
@@ -1,5 +1,10 @@
 class role::st2express {
   include ::profile::st2server
-  include ::profile::hubot
   include ::profile::users
+
+  if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
+    notify { 'Hubot is not supported on this platform': }
+  } else {
+    include ::profile::hubot
+  }
 }
