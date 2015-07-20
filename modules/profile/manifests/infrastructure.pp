e99c7a1ef33d805bfea8459634cb8fa6313fbefa
James Fryman <james@fryman.io>
2015-07-20 16:38:05 -0500

    adding hiera-based hostname from st2installer

--- modules/profile/manifests/infrastructure.pp
+++ modules/profile/manifests/infrastructure.pp
@@ -1,3 +1,27 @@
 class profile::infrastructure {
+  $_hostname = hiera('system::hostname', $::fqdn)
   include ::ntp
+
+  # Setup Hostname via Hiera
+  ## This is a bit of a Snake eating its own tail here, but
+  ## Allows us to set up a random hostname (say, with a cloud provider),
+  ## and then let the user come around and configure it with st2installer
+  file { '/etc/hostname':
+    ensure => file,
+    content => "${_hostname}\n",
+    notify  => Exec['apply hostname'],
+  }
+  exec { "apply hostname":
+    command => "/bin/hostname -F /etc/hostname",
+    unless  => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
+  }
+  host { 'default hostname v4':
+    ensure        => present,
+    name          => $_hostname,
+    host_aliases  => [
+      'localhost',
+      'localhost.localdomain',
+    ],
+    ip            => '127.0.0.1',
+  }
 }
