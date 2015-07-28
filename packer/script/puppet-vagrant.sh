cb58944cee0c6c599aa1b210b1087b2da04facc8
James Fryman <james@fryman.io>
2015-07-28 10:54:53 -0700

    setting permanent markers for datacenter/role to pre-built images

--- packer/script/puppet-vagrant.sh
+++ packer/script/puppet-vagrant.sh
@@ -4,6 +4,9 @@ export FACTER_role=st2express
 export FACTER_datacenter=vagrant
 export environment=current_working_directory
 
+echo "datacenter=${FACTER_datacenter}" > /etc/facter/facts.d/datacenter.txt
+echo "role=${FACTER_role}" > /etc/facter/facts.d/role.txt
+
 cd /opt/puppet
 script/bootstrap-linux
 script/puppet-apply
