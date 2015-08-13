489c8996e06e46ea614dc941de886676553932e4
James Fryman <james@fryman.io>
2015-08-13 11:20:18 -0500

    rely on alternatives for npm

--- modules/profile/manifests/hubot.pp
+++ modules/profile/manifests/hubot.pp
@@ -22,12 +22,6 @@ class profile::hubot(
     'hubot-hipchat',
   ]
 
-  file { '/usr/bin/node':
-    ensure => symlink,
-    target => '/usr/bin/nodejs',
-    require => Class['nodejs'],
-  }
-
   Exec<| title == 'Hubot init' |> {
     path => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
   }
