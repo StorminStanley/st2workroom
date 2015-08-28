6ee5033882343b78be5b00b1fd0c246a5dd1b3b1
James Fryman <james@fryman.io>
2015-08-28 17:27:53 -0500

    working initial boot

---
+++ modules/profile/manifests/mongodb.pp
@@ -0,0 +1,27 @@
+class profile::mongodb {
+  include ::docker
+  $_version = hiera('mongodb::version', '2.4.14')
+
+  if $::osfamily == 'Debian' {
+    # Needed to build mongoengine
+    package { 'mongodb-dev':
+      ensure => present
+    }
+  }
+
+  docker::image { 'mongo':
+    ensure    => present,
+    image_tag => $_version,
+  }
+
+  docker::run { 'mongo':
+    image   => 'mongo',
+    volumes => [
+      '/var/lib/mongodb:/data/db',
+    ],
+    ports   => [
+      '27017:27017',
+    ],
+    require => Docker::Image['mongo'],
+  }
+}
