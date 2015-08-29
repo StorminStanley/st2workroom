301777c6acf403aa5d322c577f34a2d589491ffc
James Fryman <james@fryman.io>
2015-08-29 10:05:15 -0500

    fix mongodb removal

--- modules/deprecate/manifests/os_mongodb_0001.pp
+++ modules/deprecate/manifests/os_mongodb_0001.pp
@@ -9,51 +9,20 @@
 class deprecate::os_mongodb_0001(
   $enforce = true,
 ) {
-  $_stage = $::deprecate_os_mongodb_0001_stage
+  $_enforced = $::deprecate_os_mongodb_0001_status
 
-  case $_stage {
-    undef: {
-      $_service_ensure = 'stopped'
-      $_service_enable = 'false'
-      $_package_ensure = undef
-
-      # The next stage to process
-      $_next_stage = 'stopped'
-    }
-    'stopped': {
-      $_service_ensure = 'stopped'
-      $_service_enable = 'false'
-      $_package_ensure = 'absent'
-
-      # The next stage to process
-      $_next_stage = 'uninstalled'
-    }
-    default: {
-      $_service_ensure = 'stopped'
-      $_service_enable = 'false'
-      $_package_ensure = 'absent'
-
-      $_next_stage = undef
-    }
-  }
-
-  if $enforce {
+  if ! $_enforced {
     class { '::mongodb::server':
-      service_ensure => $_service_ensure,
-      package_ensure => $_package_ensure,
+      service_ensure => 'stopped',
+      service_enable => 'false',
     }
-
-    # Set the breadcrumb for the next run.
-
-    if $_next_stage {
-      file { '/etc/facter/facts.d/deprecate_os_mongodb_0001_stage.txt':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => "deprecate_os_mongodb_0001_stage=${_next_stage}",
-        require => Class['::mongodb::server'],
-      }
+    file { '/etc/facter/facts.d/deprecate_os_mongodb_0001_stage.txt':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      content => 'deprecate_os_mongodb_0001_stage=true',
+      require => Class['::mongodb::server'],
     }
   }
 }
