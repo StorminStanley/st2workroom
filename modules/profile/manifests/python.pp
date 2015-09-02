692358fe66aeba3ba7e7ac6a6a8b114380e58ce6
James Fryman <james@fryman.io>
2015-09-02 13:59:08 -0500

    better way to trigger the update

--- modules/profile/manifests/python.pp
+++ modules/profile/manifests/python.pp
@@ -1,6 +1,15 @@
 class profile::python {
   include ::st2::profile::python
 
+  file { '/etc/facter/facts.d/pip_upgrade_20150902.txt':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'pip_upgrade_20150902=true',
+    notify  => Exec['update-pip'],
+  }
+
   exec { 'update-pip':
     command     => 'pip install -U pip',
     path        => '/usr/sbin:/usr/bin:/sbin:/bin',
