041b15323678cc1f1e2bd1defde0580732a866ee
James Fryman <james@fryman.io>
2015-09-04 07:53:09 -0500

    add ability to disable OK check/Hubot

--- modules/role/manifests/st2express.pp
+++ modules/role/manifests/st2express.pp
@@ -1,11 +1,15 @@
 class role::st2express {
+  $_enable_hubot = hiera('hubot', true)
+
   include ::profile::infrastructure
   include ::profile::st2server
   include ::profile::users
 
-  if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
-    notify { 'Hubot is not supported on this platform': }
-  } else {
-    include ::profile::hubot
+  if $_enable_hubot {
+    if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
+      notify { 'Hubot is not supported on this platform': }
+    } else {
+      include ::profile::hubot
+    }
   }
 }
