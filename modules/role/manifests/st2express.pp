a222264daf0a5b94cdf758735b9b2e66a34689f0
James Fryman <james@fryman.io>
2015-09-14 21:10:14 -0700

    include instead of class

--- modules/role/manifests/st2express.pp
+++ modules/role/manifests/st2express.pp
@@ -4,7 +4,7 @@ class role::st2express {
   include ::profile::infrastructure
   include ::profile::st2server
   include ::profile::users
-  class { '::st2migrations': }
+  include ::st2migrations
 
   if $_enable_hubot {
     if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
