6427d1c4334dabed3e4ca89eea62c11083428eba
Patrick Hoolboom <patrick@stackstorm.com>
2015-09-20 09:36:33 -0700

    Update examples.pp

--- modules/profile/manifests/examples.pp
+++ modules/profile/manifests/examples.pp
@@ -1,6 +1,7 @@
 class profile::examples {
 
   $examples_script = $osfamily ? {
+    'Debian' => '/usr/lib/python2.7/dist-packages/st2common/bin/st2-setup-examples',
     'Ubuntu' => '/usr/lib/python2.7/dist-packages/st2common/bin/st2-setup-examples',
     'RedHat' => '/usr/lib/python2.7/site-packages/st2common/bin/st2-setup-examples'
   }
