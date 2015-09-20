3244109fa8582c56cddc1d67b353ebb937a2f028
Patrick Hoolboom <patrick@stackstorm.com>
2015-09-19 23:07:36 -0700

    Create examples.pp

---
+++ modules/profile/manifests/examples.pp
@@ -0,0 +1,13 @@
+class profile::examples {
+
+  $examples_script = $osfamily {
+    'Ubuntu' => '/usr/lib/python2.7/dist-packages/st2common/bin/st2-setup-examples',
+    'RedHat' => '/usr/lib/python2.7/site-packages/st2common/bin/st2-setup-examples'
+  }
+  
+  exec {'examples':
+    path     => '/bin:/usr/bin"
+    command  => "python2.7 ${examples_script}"
+    creates  => '/opt/stackstorm/packs/examples'
+  }
+}
