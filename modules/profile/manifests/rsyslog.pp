67b03f5750ffce3884ba2baf9b775eac9e4cf79a
Patrick Hoolboom <patrick@stackstorm.com>
2015-09-21 15:47:53 -0700

    file_line hackery for rsyslog server

--- modules/profile/manifests/rsyslog.pp
+++ modules/profile/manifests/rsyslog.pp
@@ -2,11 +2,13 @@ class profile::rsyslog {
   include ::rsyslog::client
 
   file_line{'$ModLoad imudp':
-    path => '/etc/rsyslog.conf',
-    line => '$ModLoad imudp'
+    path   => '/etc/rsyslog.conf',
+    line   => '$ModLoad imudp',
+    notify => Service['rsyslog']
   }
   file_line{'$UDPServerRun 514':
     path => '/etc/rsyslog.conf',
-    line => '$UDPServerRun 514'
+    line => '$UDPServerRun 514',
+    notify => Service['rsyslog']
   }
 }
