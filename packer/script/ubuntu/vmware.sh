40fe37debd0d701df03e1ee7d34459fabb062c4b
James Fryman <james@fryman.io>
2015-09-28 17:19:33 -0500

    using open-vm-tools versus vmware-tools install

--- packer/script/ubuntu/vmware.sh
+++ packer/script/ubuntu/vmware.sh
@@ -4,20 +4,4 @@ PROJECT_ROOT=/opt/puppet
 export PATH=${PROJECT_ROOT}/bin:/usr/local/bin:$PATH
 cd $PROJECT_ROOT
 
-# Bail if we are not running inside VMWare.
-if [ $(bundle exec facter virtual) != "vmware" ]; then
-    exit 0
-fi
-
-apt-get -y install build-essential linux-headers-$(uname -r)
-
-# Install the VMware Fusion guest tools
-cd /tmp
-mkdir -p /mnt/cdrom
-mount -o loop ~/linux.iso /mnt/cdrom
-tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-/tmp/vmware-tools-distrib/vmware-install.pl -d
-rm ~/linux.iso
-umount /mnt/cdrom
-
-echo "answer AUTO_KMODS_ENABLED yes" | sudo tee -a /etc/vmware-tools/locations
+apt-get -y install open-vm-tools open-vm-tools-dkms
