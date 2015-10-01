b9930711c7fd21b0b8b1822059da68ef27681a57
James Fryman <james@fryman.io>
2015-10-01 10:33:41 -0500

    ensure SSH keys are regenerated on boot

--- packer/script/ubuntu/vsphere/cleanup.sh
+++ packer/script/ubuntu/vsphere/cleanup.sh
@@ -8,15 +8,30 @@ for user in `ls /home`; do
   rm -rf /home/$user/.ssh/authorized_keys
 done
 
+
+
 echo "Shredding SSH host key pairs..."
 shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 
-echo "Removing all user passwords..."
-for i in `cat /etc/shadow`; do
-	USER=`echo $i | awk '{split($0,a,":"); print a[1]}'`
-	PWHASH=`echo $i | awk '{split($0,a,":"); print a[2]}'`
+## Ensure Host Keys are re-generated on first boot
+echo "dpkg-reconfigure openssh-server" >> /etc/rc.local
 
-	if [[ "${PWHASH}" == \$* ]]; then
-	  passwd -l $USER
-	fi
-done
+### This must be done after password cleanup
+## During first boot, there is a period of time where
+## the machine is configuring itself for the installer
+## to be run. This user exists until StackStorm
+## is up and running
+
+# See LastPass for bootstrap password
+USER="bootstrap"
+PASSWORD_HASH='$6$fyfRT6IWkc7IYC8$JSpW3AQuM6XFtfy5lFTRoThVBSYgZiZWJvU0.5D0WpqSsyML/U3dBbX9XB1doKh1tA/73j77Ghs0jNF9adwh7.'
+
+echo "Setting failsafe bootstrap user..."
+useradd --create-home $USER
+usermod -p $PASSWORD_HASH $USER
+
+## Ensure provisioning user is locked to avoid security leak.
+## This is a bit of a hack, because the bootstrap user
+## needs access until Packer is done, so this needs to happen on
+## first boot.
+echo "usermod -L vagrant" >> /etc/rc.local
