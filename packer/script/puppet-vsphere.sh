334597176dee728670db61807bb0d909098ee36f
James Fryman <james@fryman.io>
2015-09-30 22:53:13 -0500

    ensure vsphere datacenter is preserved

--- packer/script/puppet-vsphere.sh
+++ packer/script/puppet-vsphere.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export FACTER_role=st2enterprise
-export FACTER_datacenter=atlas
+export FACTER_datacenter=vsphere
 export environment=current_working_directory
 
 echo "role=${FACTER_role}" > /etc/facter/facts.d/role.txt
@@ -10,4 +10,4 @@ cd /opt/puppet
 script/bootstrap-linux
 script/puppet-apply
 
-echo "datacenter=vsphere" > /etc/facter/facts.d/datacenter.txt
+echo "datacenter=${FACTER_datacenter}" > /etc/facter/facts.d/datacenter.txt
