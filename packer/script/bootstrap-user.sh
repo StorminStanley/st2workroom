60da91637a3ca5d5bfc5e60fbd77458632d94808
James Fryman <james@fryman.io>
2015-09-30 20:25:05 -0500

    secure password hash

--- packer/script/bootstrap-user.sh
+++ packer/script/bootstrap-user.sh
@@ -2,7 +2,11 @@
 
 ## This script creates a bootstrap user to be used while initial machines are
 ## provisioning. Will be removed by first Puppet run.
-USER=bootstrap
+## See Class['::profile::infrastructure']
+
+# See LastPass for bootstrap password
+USER="bootstrap"
+PASSWORD_HASH='$6$fyfRT6IWkc7IYC8$JSpW3AQuM6XFtfy5lFTRoThVBSYgZiZWJvU0.5D0WpqSsyML/U3dBbX9XB1doKh1tA/73j77Ghs0jNF9adwh7.'
 
 useradd --create-home $USER
-echo "st@ckst0rm" | passwd "$USER" --stdin
+usermod -p $PASSWORD_HASH $USER
