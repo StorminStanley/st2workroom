2cfd2c326407ea9d8e7b987110a15ecce4505f41
James Fryman <james@fryman.io>
2015-10-05 13:39:23 -0700

    adding rbac to right place

--- modules/profile/manifests/st2server.pp
+++ modules/profile/manifests/st2server.pp
@@ -228,28 +228,6 @@ class profile::st2server {
     }
   }
 
-  # Create default admin role assignment for root_cli user
-  st2::rbac { $_root_cli_username:
-    description  => 'Default admin role assignments created by the installer',
-    roles        => [
-        'admin'
-    ]
-  }
-
-  # Create default system_admin role assignment for admin user created during installation
-  # Note: Assignment is only created once the installer has completed.
-  $_users = hiera_hash('users', {}).keys()
-  if size($_users) >= 1 {
-      $_admin_user = $_users[0]
-
-      st2::rbac { $_admin_user:
-        description  => 'Default system_admin role assignments created by the installer',
-        roles        => [
-            'system_admin'
-        ]
-      }
-  }
-
   anchor { 'st2::pre_reqs': }
   class { '::st2::profile::client':
     username             => $_root_cli_username,
