df589fd55bc3293b5e5fed90487725d2cef1e86e
Patrick Hoolboom <patrick@stackstorm.com>
2015-10-07 13:31:02 -0700

    Adding systemd templates

---
+++ modules/st2_systemd/manifests/startup.pp
@@ -0,0 +1,19 @@
+# Definition: st2_systemd
+#
+#  Lets create some Systemd scripts
+#
+define st2_systemd::startup (
+  $st2_process  = undef,
+  $process_type = single
+  ) {
+
+  file{"/etc/systemd/system/${st2_process}.service":
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    content => template("modules/st2_systemd/st2service_${process_type}.service.erb"),
+    notify  => Service["${st2_process"],
+  }
+
+}
