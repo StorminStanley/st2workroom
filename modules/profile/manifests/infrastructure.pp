8eaca96be9f793a051cd079faa76f7bc3bb8eeee
James Fryman <james@fryman.io>
2015-10-06 19:10:17 -0700

    still set the hostname

--- modules/profile/manifests/infrastructure.pp
+++ modules/profile/manifests/infrastructure.pp
@@ -1,12 +1,27 @@
 class profile::infrastructure {
   $_packages = hiera('system::packages', [])
   $_offline_mode = hiera('system::offline_mode', false)
+  $_fqdn = hiera('system::hostname', $::fqdn)
 
   include ::ntp
   include ::profile::rsyslog
 
   package { $_packages:
     ensure => present,
+  }
+
+  # Setup Hostname via Hiera
+  ## This is a bit of a Snake eating its own tail here, but
+  ## Allows us to set up a random hostname (say, with a cloud provider),
+  ## and then let the user come around and configure it with st2installer
+  file { '/etc/hostname':
+    ensure => file,
+    content => "${_fqdn}\n",
+    notify  => Exec['apply hostname'],
+  }
+  exec { "apply hostname":
+    command => "/bin/hostname -F /etc/hostname",
+    unless  => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
   }
 
   # Set offline State
