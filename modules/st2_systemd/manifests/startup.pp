5bb02ecaa678eaa2bea293d569d92458fc4ead6a
Patrick Hoolboom <patrick@stackstorm.com>
2015-10-07 13:39:42 -0700

    Added systemctl enable

--- modules/st2_systemd/manifests/startup.pp
+++ modules/st2_systemd/manifests/startup.pp
@@ -12,14 +12,19 @@ define st2_systemd::startup (
   } else {
     $type = ''
   }
-  
+
   file{"/etc/systemd/system/${st2_process}${type}.service":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0444',
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
     content => template("modules/st2_systemd/st2service_${process_type}.service.erb"),
     notify  => Service["${st2_process}"],
   }
 
+  exec{'sysctl enable':
+    path    => '/bin:/usr/bin:/usr/local/bin',
+    command => "systemctl --no-reload enable ${st2_process}",
+    require => File["/etc/systemd/system/${st2_process}${type}.service"]
+  }
 }
