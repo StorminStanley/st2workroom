05452aec24dfa97c9f85c3568ff519a335a85e59
James Fryman <james@fryman.io>
2015-10-12 00:36:13 -0500

    add all defaults for python

--- modules/profile/manifests/python.pp
+++ modules/profile/manifests/python.pp
@@ -21,13 +21,20 @@ class profile::python {
   Exec['update-pip'] -> Python::Pip<||>
   Class['::st2::profile::repos'] -> Class['::st2::profile::python']
 
+  # RedHad 6 uses Python 2.6 by default, and we are pulling upstream
+  # from IUS to provide Python 2.7. The following sets up latest Python
+  # to be the System Python
   if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
+    alternatives { 'python':
+      path    => '/usr/bin/python2.7',
+      require => Class['::st2::profile::python'],
+    }
     alternatives { 'virtualenv':
       path    => '/usr/bin/virtualenv-2.7',
       require => Class['::st2::profile::python'],
     }
     alternatives { 'pip':
-      path    => '/usr/bin/pip-2.7',
+      path    => '/usr/bin/pip2.7',
       require => Class['::st2::profile::python'],
     }
 
