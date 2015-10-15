25fb3b8377506c9d45975a557f2f8381b1cabee2
Dmitri Zimin(e) <dz@stackstorm.com>
2015-10-14 22:59:06 -0700

    Revert "RFR: Add pam auth backend profile"

--- modules/role/manifests/st2express.pp
+++ modules/role/manifests/st2express.pp
@@ -3,16 +3,8 @@ class role::st2express {
 
   include ::profile::infrastructure
   include ::profile::st2server
-  include ::profile::auth_backend_pam
   include ::profile::users
   include ::st2migrations
-
-  st2::helper::auth_manager {
-    auth_mode     => 'standalone',
-    auth_backend  => 'pam',
-    debug         => false,
-    test_user     => false
-  }
 
   if $_enable_hubot {
     include ::profile::hubot
