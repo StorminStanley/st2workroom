9319679888f782b95d5805dcd74765519a4f1ae8
James Fryman <james@fryman.io>
2015-11-25 14:17:48 -0600

    adding safe feature-toggle for hubot migration

--- modules/profile/manifests/hubot.pp
+++ modules/profile/manifests/hubot.pp
@@ -1,60 +1,9 @@
-class profile::hubot(
-  $enable         = true,
-  $bot_name       = 'hubot',
-  $version        = undef,
-  $docker_image   = 'stackstorm/hubot',
-  $data_container = 'hubotdata',
-  $http_port      = '8081',
-  ) {
+class profile::hubot{
+  $_hubot_docker = hiera('hubot::docker', false)
 
-  # Get ENV vars hash from Hiera, and map to an array for Docker
-  $_adapter         = hiera('hubot::adapter', 'shell')
-  $_alias           = hiera('hubot::chat_alias', '!')
-  $_hiera_env_vars  = hiera_hash('hubot::env_export', {})
-  $_user_env_vars   = {
-    "HUBOT_NAME"    => $bot_name,
-    "HUBOT_ADAPTER" => $_adapter,
-    "HUBOT_ALIAS"   => $_alias,
-  }
-  $_all_env_vars    = merge($_hiera_env_vars, $_user_env_vars)
-  $_docker_env_vars = $_all_env_vars.map |$_key, $_value| {
-    "${_key}=${_value}"
-  }
-  $_run_image = $version ? {
-    undef   => $docker_image,
-    default => "${docker_image}:${version}",
-  }
-
-  if ! defined(Class['docker']) {
-    fail('[profile::hubot]: Docker is not enabled on this host.')
-  }
-
-  if $enable {
-    ## Download main docker image for Hubot
-    docker::image { $docker_image:
-      ensure    => present,
-      image_tag => $version,
-    }
-
-    docker::run { 'hubot':
-      image            => $_run_image,
-      env              => $_docker_env_vars,
-      ports            => [
-        "${http_port}:8080",
-      ],
-      extra_parameters => ['--restart=always'],
-      require          => [
-        Docker::Image[$docker_image],
-      ],
-    }
-  }
-
-  # Some hubot adapters are flakey, and randomly die.
-  # This is a workaround until upstream PRs are merged.
-  cron { 'restart hubot':
-    command => "service docker-hubot restart",
-    user    => 'root',
-    hour    => '*/12',
-    require => Docker::Run['hubot'],
+  if $_hubot_docker {
+    include ::profile::hubot::docker
+  } else {
+    include ::profile::hubot::legacy
   }
 }
